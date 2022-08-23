7a7ff6dc2bc27a76da0f0b532ea59eb695f0b625
Mehmet Ali Karakas <mali.karakas11@gmail.com>
2022-08-23 13:22:19 +0300

    add create pages

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import { useState } from "react";
 
 const Create = () => {
+  const [baslik, setBaslik] = useState("");
+  const [aciklama, setAciklama] = useState("");
+  const [yazar, setYazar] = useState("");
+  const [yukleniyor, setYukleniyor] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setYukleniyor(true);
+    const yazi = { ad: baslik, aciklama, yazar };
+    fetch("http://localhost:8000/yazilar", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(yazi),
+    }).then(() => {
+      console.log("yazi eklemdi");
+      setYukleniyor(false);
+    });
+  };
+
   return (
     <div className="create">
       <h2 style={{ color: "#ff793f" }}>Yeni Yazı Ekle</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>Yazi Baslik : </label>
+        <input
+          type="text"
+          required
+          value={baslik}
+          onChange={(e) => setBaslik(e.target.value)}
+        />
+        <label>Yazı Aciklama : </label>
+        <textarea
+          required
+          value={aciklama}
+          onChange={(e) => setAciklama(e.target.value)}
+        ></textarea>
+        <label>Yazar : </label>
+        <select value={yazar} onChange={(e) => setYazar(e.target.value)}>
+          <option value="Veli Demir">Veli Demir</option>
+          <option value="Ahmet Yılmaz">Ahmet Yılmaz</option>
+          <option value="Mehmet Altın">Mehmet Altın</option>
+          <option value="Ali Kaya">Ali Kaya</option>
+          <option value="Sina Kural">Sina Kural</option>
+        </select>
+        {!yukleniyor && <button>Ekle</button>}
+        {yukleniyor && <button disabled>Yükleniyor...</button>}
+      </form>
     </div>
   );
 };
